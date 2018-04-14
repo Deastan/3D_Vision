f7676b1ea2e364823ae30b5bc56904eeb8e12275
philipp <goeldlin@hotmail.ch>
2018-04-14 22:10:56 +0200

    now ellipses instead of circles also using dilation now

--- Codes/Background_Phil/ConnectedComponentsWithStats_Video.py
+++ Codes/Background_Phil/ConnectedComponentsWithStats_Video.py
@@ -2,11 +2,12 @@ import numpy as np
 import cv2
 import time
 
-cap = cv2.VideoCapture('/home/philipp/Desktop/ruche.wmv')
+cap = cv2.VideoCapture('/home/philipp/Desktop/pictures_and_videos/Avril/GOPR1329.MP4')
 fourcc = cv2.VideoWriter_fourcc(*'XVID') # Define the codec and create VideoWriter object (fourcc)
 init=0
 fgbg = cv2.createBackgroundSubtractorMOG2()
 font = cv2.FONT_HERSHEY_SIMPLEX
+counter=0
 
 
 
@@ -17,6 +18,11 @@ while(1):
     fgmask = fgbg.apply(original)
     fgmask= cv2.medianBlur(fgmask, 9)
     ret, fgmask = cv2.threshold(fgmask, 120, 255, cv2.THRESH_BINARY)
+
+    kernel = np.ones((40,40),np.uint8)
+    # erosion = cv2.erode(fgmask,kernel,iterations = 1)
+    fgmask = cv2.dilate(fgmask,kernel,iterations = 1)
+
 
 # You need to choose 4 or 8 for connectivity type
     connectivity = 8
@@ -34,22 +40,35 @@ while(1):
     fgmask=cv2.cvtColor(fgmask, cv2.COLOR_GRAY2BGR)
     j=0
     for i in range(1,num_labels): #don't do 0, cause it's just the background
-        if stats[i,4]>400: #threshold to filter out small patches
+        if stats[i,4]>2500: #threshold to filter out small patches
             j+=1
-            cv2.circle(original,(int(centroids[i,0]),int(centroids[i,1])), 30, (0,0,255), 2)
-            cv2.putText(original,str(j),(int(centroids[i,0]),int(centroids[i,1])), font, 1,(0,255,0),2,cv2.LINE_AA)
+            # cv2.circle(original,(int(centroids[i,0]),int(centroids[i,1])), stats[i,4]//30, (0,0,255), 2)
+            # cv2.putText(original,str(j),(int(centroids[i,0]),int(centroids[i,1])), font, 1,(0,255,0),2,cv2.LINE_AA)
 
+            # cv2.circle(original,(int(centroids[i,0]),int(centroids[i,1])), 30, (0,0,255), 2)
+            cv2.ellipse(original,(int(centroids[i,0]),int(centroids[i,1])),(stats[i,2]//3,stats[i,3]//3),0,0,360,255,2)
+            cv2.putText(original,str(j),(int(centroids[i,0]),int(centroids[i,1])), font, 1,(0,255,0),2,cv2.LINE_AA)
 
 
     cv2.namedWindow('frame_median',cv2.WINDOW_NORMAL)
     cv2.resizeWindow('frame_median', 1200,800)
+
     cv2.imshow('frame_median', original)
-    time.sleep(0.8)
+
+    # labels=labels*50000
+    # cv2.imshow('frame_median', labels)
 
     if init==0:
         out = cv2.VideoWriter('/home/philipp/Desktop/video_circle.avi',fourcc, 10,(original.shape[1],original.shape[0])) #define: format, fps, and frame-size (pixels)
         init=1
     out.write(original)
+    #
+    # if counter==3:
+    #     cv2.imwrite('/home/philipp/Desktop/fgmask.jpg',fgmask)
+
+    counter+=1
+    print ("\r frame"+str(counter),end= "")
+
 
     k = cv2.waitKey(1) & 0xff#modify the frame-speed
     if k == 27:
