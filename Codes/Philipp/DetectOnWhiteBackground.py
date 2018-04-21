85027bba1442538be39f8ed3491dfb98136f5437
philipp <goeldlin@hotmail.ch>
2018-04-21 15:48:46 +0200

    made some optimizations

--- Codes/Philipp/DetectOnWhiteBackground.py
+++ Codes/Philipp/DetectOnWhiteBackground.py
@@ -14,14 +14,18 @@ arraySearch = False
 ######################################################################################################
 
 
-def createHistoArray(frameNumber, labelNumber):
-    global blueArray_current, greenArray_current, redArray_current
-    if frameNumber==frameNumber: #TODO: wtf???
+histoBlue_current=[0 for x in range(255)]
+histoGreen_current=[0 for x in range(255)]
+histoRed_current=[0 for x in range(255)]
+
+
+def createHistoArray(frameTolookAt, labelNumber):
+    global histoBlue_current, histoGreen_current, histoRed_current
+    if frameTolookAt==frameNumber:
         cv2.destroyAllWindows()
-        Utilities.showCaughtPatch(realOriginal, labels, labelNumber)
+        # Utilities.showCaughtPatch(realOriginal, labels, labelNumber)
 
-        blueArray_current, greenArray_current, redArray_current = Utilities.getHistogram(labelNumber, realOriginal)
-
+        histoBlue_current,histoGreen_current, histoRed_current = Utilities.getHistogramForArraySearch(labelNumber,histoBlue_current,histoGreen_current, histoRed_current, realOriginal)
 
 
 while (1):
@@ -48,16 +52,16 @@ while (1):
 
 
     # everything concerning showing the window
-    if arraySearch==False:
-        cv2.putText(original, "frame:" +str(frameNumber),(50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
-        cv2.namedWindow('frame_median', cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('frame_median', 1200, 800)
-        cv2.imshow('frame_median', original)
+    # if arraySearch==False:
+    #     cv2.putText(original, "frame:" +str(frameNumber),(50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+    #     cv2.namedWindow('frame_median', cv2.WINDOW_NORMAL)
+    #     cv2.resizeWindow('frame_median', 1200, 800)
+    #     cv2.imshow('frame_median', original)
 
 
 
     if frameNumber == 1:
-        out = cv2.VideoWriter('/home/philipp/Desktop/ellipses_without_histo.avi', fourcc, 15, (original.shape[1], original.shape[0]))  # define: format, fps, and frame-size (pixels)
+        out = cv2.VideoWriter('/home/philipp/Desktop/ellipses.avi', fourcc, 15, (original.shape[1], original.shape[0]))  # define: format, fps, and frame-size (pixels)
 
     out.write(original)
     print("\r frame" + str(frameNumber), end="")
@@ -71,21 +75,25 @@ while (1):
 
 
     #only for static-Array-creation
+    #DO NOT TAKE TWO OUT OF THE SAME FRAME!!!
     if arraySearch == True:
-        createHistoArray(2,6)
-        createHistoArray(5,1)
-        createHistoArray(6,3)
         createHistoArray(7,4)
-        createHistoArray(10,2)
-        createHistoArray(21,7)
-        if frameNumber ==21: #put the last frame that was used!
-            blueArray_current/=6#divide by the number of frames!
-            greenArray_current/=6
-            redArray_current/=6
+        createHistoArray(9,1)
+        createHistoArray(12,2)
+        createHistoArray(13,3)
+        createHistoArray(15,3)
+        createHistoArray(40,1)
+        if frameNumber ==40: #put the last frame that was used!
+            NumberOfFrames = 6
+            histoBlue_current=[x/NumberOfFrames for x in histoBlue_current]#divide by the number of frames!
+            histoGreen_current=[x/NumberOfFrames for x in histoGreen_current]
+            histoRed_current=[x/NumberOfFrames for x in histoRed_current]
 
-            print("\nblue:\n", blueArray_current)
-            print("green:\n", greenArray_current)
-            print("red:\n", redArray_current)
+            print("\nblue:\n", histoBlue_current)
+            print("green:\n", histoGreen_current)
+            print("red:\n", histoRed_current)
+
+            print("!!!!did you not forget to modify the number of bees and the final framenumber???")
             break
 
 
