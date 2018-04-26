9156b30a573099bc95e72b1b674e8a27d0082ed6
juveya <juveya@student.ethz.ch>
2018-04-26 18:42:44 +0200

    jf

--- Codes/Main/Examples/exampleUseBee.py
+++
@@ -1,9 +0,0 @@
-from Bee import Bee
-
-# How use a bee ?
-# short example
-
-bee01 = Bee(1, 0, 0, 1, 2) # create a bee
-bee01.screen() # screen the information
-bee01.newPosition(1,2) # change the position of the bee
-bee01.screen()
