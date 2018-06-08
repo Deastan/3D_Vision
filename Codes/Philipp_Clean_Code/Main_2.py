7fa8ae7a49370b613559f4af9ecbe8a77b979c0b
philipp <goeldlin@hotmail.ch>
2018-06-08 22:28:15 +0200

    save

--- Codes/Philipp_Clean_Code/Main_2.py
+++ Codes/Philipp_Clean_Code/Main_2.py
@@ -19,27 +19,18 @@ cap = cv2.VideoCapture(videoPath)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Define the codec and create VideoWriter object (fourcc)
 fgbg = cv2.createBackgroundSubtractorMOG2(1, 10)
 
-arraySearch = False
-squareEntrance =True
 lineHistory =[]
 
 histoBlue_current=[0 for x in range(256)]
 histoGreen_current=[0 for x in range(256)]
 histoRed_current=[0 for x in range(256)]
 beesLastFrame =[]
-######################################################################################################
-
-
-
 
 sumBeesIn = 0
 sumBeesOut= 0
+######################################################################################################
 
-def createHistoArray(frameTolookAt, labelNumber):
-    global histoBlue_current, histoGreen_current, histoRed_current
-    if frameTolookAt==frameNumber:
-        cv2.destroyAllWindows()
-        histoBlue_current,histoGreen_current, histoRed_current = Utilities.getHistogramForArraySearch(labelNumber,histoBlue_current,histoGreen_current, histoRed_current, realOriginal)
+
 
 
 
@@ -50,6 +41,10 @@ while (1):
     if ret==False:
         print('File not found')
         break
+
+
+
+    #Background-Subtraction and filtering on the image
     realOriginal=np.array(original)
     fgmask = fgbg.apply(original)
     fgmask = cv2.medianBlur(fgmask, 9)
@@ -63,17 +58,12 @@ while (1):
 
 
 
-
-
-
     if frameNumber!=1:
         labels, beesCurrentFrame=Utilities.connectedComponents(fgmask, original, realOriginal)
         if frameNumber>=2:
             beesIn, beesOut = Utilities.counter(beesCurrentFrame, beesLastFrame, original, lineHistory)
             sumBeesIn+=beesIn
             sumBeesOut+=beesOut
-            # cv2.putText(original, "In:" +str(beesIn),(50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-            # cv2.putText(original, "Out:" +str(beesOut),(50, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
             cv2.putText(original, "In_total:" +str(sumBeesIn),(50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
             cv2.putText(original, "Out_total:" +str(sumBeesOut),(50, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
@@ -82,21 +72,16 @@ while (1):
 
 
 
-
-
-
     # everything concerning showing the window
-    if arraySearch==False:
-        cv2.putText(original, "frame:" +str(frameNumber),(50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
-        cv2.namedWindow('Bee recognition', cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('Bee recognition', 1200, 800)
-        cv2.line(original,(80,570),(1840,570),(255,0,0),5)
-        if squareEntrance ==True:
-            cv2.line(original,(80,1000),(1840,1000),(255,0,0),5)
-            cv2.line(original,(80,570),(80,1000),(255,0,0),5)
-            cv2.line(original,(1840,570),(1840,1000),(255,0,0),5)
+    cv2.putText(original, "frame:" +str(frameNumber),(50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+    cv2.namedWindow('Bee recognition', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Bee recognition', 1200, 800)
+    cv2.line(original,(80,570),(1840,570),(255,0,0),5)
+    cv2.line(original,(80,1000),(1840,1000),(255,0,0),5)
+    cv2.line(original,(80,570),(80,1000),(255,0,0),5)
+    cv2.line(original,(1840,570),(1840,1000),(255,0,0),5)
 
-        cv2.imshow('Bee recognition', original)
+    cv2.imshow('Bee recognition', original)
 
 
 
@@ -104,37 +89,6 @@ while (1):
     if frameNumber == 1:
         out = cv2.VideoWriter('/home/philipp/Desktop/ellipses.avi', fourcc, 4, (original.shape[1], original.shape[0]))  # define: format, fps, and frame-size (pixels)
     out.write(original)
-
-
-
-
-
-
-
-    #only for static-Array-creation
-    #DO NOT TAKE TWO OUT OF THE SAME FRAME!!!
-    if arraySearch == True:
-        createHistoArray(7,4)
-        createHistoArray(9,1)
-        createHistoArray(12,2)
-        createHistoArray(13,3)
-        createHistoArray(15,3)
-        createHistoArray(40,1)
-        if frameNumber ==40: #put the last frame that was used!
-            NumberOfFrames = 6
-            histoBlue_current=[x/NumberOfFrames for x in histoBlue_current]#divide by the number of frames!
-            histoGreen_current=[x/NumberOfFrames for x in histoGreen_current]
-            histoRed_current=[x/NumberOfFrames for x in histoRed_current]
-
-            print("\nblue:\n", histoBlue_current)
-            print("green:\n", histoGreen_current)
-            print("red:\n", histoRed_current)
-
-            print("!!!!did you not forget to modify the number of bees and the final framenumber???")
-            break
-
-
-
 
 
     k = cv2.waitKey(1) & 0xff  # modify the frame-speed
