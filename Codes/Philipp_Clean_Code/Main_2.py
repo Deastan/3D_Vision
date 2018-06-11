f552346ca34a3ee9931c60c539beac671f119f67
philipp <goeldlin@hotmail.ch>
2018-06-11 20:45:26 +0200

    save

--- Codes/Philipp_Clean_Code/Main_2.py
+++ Codes/Philipp_Clean_Code/Main_2.py
@@ -2,9 +2,6 @@ import numpy as np
 from Utilities import Utilities
 import cv2
 
-######################################################################################################
-
-
 #Define the input video-path
 videoPath = '/home/philipp/Desktop/GOPR1402.MP4'
 #Define the path for saving the output-video
@@ -16,31 +13,26 @@ fourcc = cv2.VideoWriter_fourcc(*'XVID')
 fgbg = cv2.createBackgroundSubtractorMOG2(1, 10)
 
 
-# Initializations:
+# Counter-Initializations:
 frameNumber = 0
 sumBeesIn = 0
 sumBeesOut= 0
-
-beesLastFrame =[]
-
-
 ######################################################################################################
-
 
 
 while (1):
     frameNumber +=1
-
+    #Read a new frame or leave the loop if the file is not found
     ret, original = cap.read()
     if ret==False:
         print('File not found')
         break
 
 
-    #Creating a copy of the image, which will never by changed (no ellipses added etc.)
+    #Create a copy of the image, which will never by changed (no ellipses added etc.) Important to have an unchanged copy for the color histogram
     realOriginal=np.array(original)
 
-    #Background-Subtraction and filtering on the image
+    #Background-Subtraction and filtering of the binary image to reduce noise, improve performance and enhance the result
     fgmask = fgbg.apply(original)
     fgmask = cv2.medianBlur(fgmask, 9)
     ret, fgmask = cv2.threshold(fgmask, 120, 255, cv2.THRESH_BINARY)
@@ -58,20 +50,18 @@ while (1):
     #Starting from the second frame (because there is no BG-subtraction in the first one)
     else:
         #Use connected-components to get a list of the individual bees.
-        beesCurrentFrame=Utilities.connectedComponents(fgmask, original, realOriginal)
-        #Use the counting function to get the bee flow of this frame
-        beesIn, beesOut = Utilities.counter(beesCurrentFrame, beesLastFrame, original)
+        Utilities.connectedComponents(fgmask, original, realOriginal)
+        #Use the counter function to get the bee flow of this frame
+        beesIn, beesOut = Utilities.counter(original)
+        #Sum it up to the total beeflow
         sumBeesIn+=beesIn
         sumBeesOut+=beesOut
+        #Display the numbers
         cv2.putText(original, "In_total:" +str(sumBeesIn),(50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
         cv2.putText(original, "Out_total:" +str(sumBeesOut),(50, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
-        #Update beesLastFrame for the next iteration
-        beesLastFrame = beesCurrentFrame
 
-
-
-    #Adding frame number and entrance-lines to the image.
+    #Display frame number and entrance-boundary-lines to the image.
     cv2.putText(original, "frame:" +str(frameNumber),(50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
     cv2.namedWindow('Bee recognition', cv2.WINDOW_NORMAL)
     cv2.resizeWindow('Bee recognition', 1200, 800)
